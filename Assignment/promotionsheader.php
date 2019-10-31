3e116f04c3471308831fdef5a43d6003860b3832
aaron <aaron.gangers@gmail.com>
2019-10-31 16:48:51 +0800

    Final - headers changed

---
+++ Assignment/promotionsheader.php
@@ -0,0 +1,33 @@
+<nav>
+            <ul class = "navigationbar">
+                <li class="navitem"><strong><a href = "index.php"><img src = "images/homeIcon.png" width = "23px" height="20px"></a></strong></li>
+                <li class="navitem"><strong><a href = "ntuclassics.php">Menu</a></strong></li>
+                <li class="navitem"><strong><a href = "promotions.php">Promotions</a></strong></li>
+                <li class="navitem"><strong><a href = "locateus.php">Locate Us</a></strong></li>
+                <li><strong><a href = "review.php">Review</a></strong></li>
+            </ul>
+        </nav>
+        <div>
+                <div class = "dropdown">
+                 <strong><a id = "signuplabel" class = "droptxt" href="signUpLogin.php"><?php if(!isset($_SESSION)){session_start();}if(!isset($_SESSION['loggedinUser']['fullname'])){echo "Sign Up/Login";}else{echo "Welcome: ".$_SESSION['loggedinUser']['fullname'];}?></a></strong>
+                 <div class = "dropdown-content">
+                     <a href="signUpLogin.php">Sign Up</a>
+                     <a href="logout.php"><?php if(!isset($_SESSION['loggedinUser'])){echo "Log In";}else{ echo "Log Out";}?></a>
+                     <a href="ntuclassics.php">Order Now</a>
+                     <a href="cartController.php">My Cart</a>
+                     <a href="admin.php">Admin</a>
+                </div>
+            </div>
+                  <form id = "ordernowlabel" action = "promotionscart.php" method="POST"><input type = "image" id = "cartbutton" src = "images/button_go-to-cart.png" alt = "cartbutton" width = "150px" height="40px">
+            </div>
+            <div>
+               <br><img class = "companyLogo" src = "images/logo.png" alt = "NTU Pizzeria" width = "100px" height="120px">
+            </div>
+            <div class = "container">
+                    <nav>
+                    <ul class = "menunavigationbar">
+                        <li class="menunavitem"><strong><a id = "menunavitem" href = "promotions.php">Our Deals</a></strong></li>
+                        <li class="menunavitem"><strong><a id = "menuitemnoborder" href = "promotions2.php">Absolute Steals</a></strong></li>
+                    </ul>
+                    </nav>
+                </div>
--- Assignment/signUpLogin.php
+++ Assignment/signUpLogin.php
@@ -9,29 +9,9 @@
 </head>
 <body>
     <header>
-            <ul class = "navigationbar">
-                <li class="navitem"><strong><a href = "index.php"><img src = "images/homeIcon.png" width = "23px" height="20px"></a></strong></li>
-                <li class="navitem"><strong><a href = "ntuclassics.php">Menu</a></strong></li>
-                <li class="navitem"><strong><a href = "promotions.php">Promotions</a></strong></li>
-                <li class="navitem"><strong><a href = "locateus.php">Locate Us</a></strong></li>
-                <li><strong><a href = "review.php">Review</a></strong></li>
-            </ul>
-            <div>
-                <div class = "dropdown">
-                 <strong><a id = "signuplabel" class = "droptxt" href="signUpLogin.php"><?php if(!isset($_SESSION)){session_start();}if(!isset($_SESSION['loggedinUser']['fullname'])){echo "Sign Up/Login";}else{echo "Welcome: ".$_SESSION['loggedinUser']['fullname'];}?></a></strong>
-                 <div class = "dropdown-content">
-                     <a href="signUpLogin.php">Sign Up</a>
-                     <a href="logout.php"><?php if(!isset($_SESSION['loggedinUser'])){echo "Log In";}else{ echo "Log Out";}?></a>
-                     <a href="ntuclassics.php">Order Now</a>
-                     <a href="cartController.php">My Cart</a>
-                     <a href="admin.php">Admin</a>
-                </div>
-            </div>
-                 <a id = "ordernowlabel" href="ntuclassics.php"><img src = "images/ordernow.png" width = 150px height = 40px></a>
-            </div>
-            <div>
-               <br><img class = "companyLogo" src = "images/logo.png" alt = "NTU Pizzeria" width = "100px" height="120px">
-            </div>
+            <?php
+            include 'headerOrderNow.php';
+            ?> 
     </header>
     <div class="row">
         <div class="column" id = "signinbox">
